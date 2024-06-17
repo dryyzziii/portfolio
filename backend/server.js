58aa99f64e935ba7fd4b5a88cb95dbf0bacd0ebb
Charles <charlesdenner10@gmail.com>
2024-06-17 20:27:14 +0200

    modif

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,3 @@ app.post('/send-email', (req, res) => {
   });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
--- frontend/src/app/services/email.service.ts
+++ frontend/src/app/services/email.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 interface EmailData {
   fullName: string;
@@ -13,7 +14,7 @@ interface EmailData {
 })
 export class EmailService {
 
-  private apiUrl = 'http://localhost:3000/send-email';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
