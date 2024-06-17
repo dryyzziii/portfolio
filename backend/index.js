09f5578847ac5c51ba0a6743dfb904e835270161
Charles <charlesdenner10@gmail.com>
2024-06-17 21:13:10 +0200

    modif

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
-const app = require('./server');
+// index.js
+import http from 'http';
 
-module.exports = app;
+// Create a server object
+const server = http.createServer((req, res) => {
+	// Set the response header
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	// Write some text to the response
+	res.end('Welcome to my simple Node.js app!');
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server
+server.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
